aa1a69ff850723b32f10f7694eb0848ebd575c2e
PetroYurchuk <petroyurchuk66@gmail.com>
2024-09-23 20:21:49 +0300

    change href on button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { scroller } from "react-scroll";
+// import { scroller } from "react-scroll";
 
 type ButtonProps = {
   children: React.ReactNode;
@@ -10,20 +10,21 @@ type ButtonProps = {
 };
 
 const Button: React.FC<ButtonProps> = ({ children, pathScroll, styles }) => {
-  const handleScroll = () => {
-    scroller.scrollTo(pathScroll, {
-      smooth: true,
-      duration: 500,
-    });
-  };
+  // const handleScroll = () => {
+  //   scroller.scrollTo(pathScroll, {
+  //     smooth: true,
+  //     duration: 500,
+  //   });
+  // };
 
   return (
-    <button
+    <a
       className={`text-xl px-4 py-2  flex justify-center items-center bg-gradient-to-b from-green-600 min-w-[300px] md:text-2xl uppercase font-semibold tracking-widest rounded-xl cursor-pointer transition-all duration-150 hover:bg-gradient-to-t hover:tracking-[2px] outline-none ${styles}`}
-      onClick={handleScroll}
+      href="tel:+380 68 479 20 78"
+      // onClick={handleScroll}
     >
       {children}
-    </button>
+    </a>
   );
 };
 export default Button;
